fca35a40baae39ab5067f4cba627585451363091
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2020-10-25 02:45:33 +0300

    Add files via upload

---
+++ asal sayı.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class AsalSayi{
+    public static void main(String[] args) {        
+ 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Üst Deðer Limiti Giriniz : ");
+        int deger = scan.nextInt();
+        int kontrol = 0;
+ 
+        if(deger<2){
+            System.out.println("En kucuk asal sayi 2 dir.");
+        }
+ 
+        for(int i=2; i<=deger;i++){
+             
+            for(int j=2;j<i;j++){                
+ 
+                if(i%j == 0){
+                    kontrol = 1;
+                }
+            }
+ 
+            if(kontrol == 0){
+               System.out.println(i);
+            }else{
+               kontrol = 0; 
+            }     
+        }
+    }
+}
