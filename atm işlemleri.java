0723a30a00c825dd7a404309b6f59d3bf3421acb
mustafadag525 <55752233+mustafadag525@users.noreply.github.com>
2020-12-18 02:11:42 +0300

    Rename atm işlemleri.c to atm işlemleri.java

--- atm işlemleri.java
+++ atm işlemleri.java
@@ -9,34 +9,34 @@ public class Main {
 
         Scanner scanner=new Scanner(System.in);
        int bakiye=1000;
-       String işlemler="1. bakiye  öğrenme\n" +
-               "2. para çekme\n" +
-               "3. para yatırma\n" +
-               "4. çıkış (çıkış ıçin q ya bas";
+       String iÅŸlemler="1. bakiye  Ã¶ÄŸrenme\n" +
+               "2. para Ã§ekme\n" +
+               "3. para yatÄ±rma\n" +
+               "4. Ã§Ä±kÄ±ÅŸ (Ã§Ä±kÄ±ÅŸ Ä±Ã§in q ya bas";
         System.out.println("----------------------");
-        System.out.println(işlemler);
+        System.out.println(iÅŸlemler);
         System.out.println("-----------------------");
 
         while (true)
         {
-            System.out.print("işlem seç: ");
-            işlemler=scanner.nextLine();
-            if(işlemler.equals("q"))
+            System.out.print("iÅŸlem seÃ§: ");
+            iÅŸlemler=scanner.nextLine();
+            if(iÅŸlemler.equals("q"))
             {
-                System.out.println("programdan çıkılıyor..");
-                System.out.println("kullanıdığınız için teşekkür ederiz...");
+                System.out.println("programdan Ã§Ä±kÄ±lÄ±yor..");
+                System.out.println("kullanÄ±dÄ±ÄŸÄ±nÄ±z iÃ§in teÅŸekkÃ¼r ederiz...");
                 break;
             }
-            else if(işlemler.equals("1"))
+            else if(iÅŸlemler.equals("1"))
             {
                 System.out.println("bakiyeniz: "+bakiye);
             }
-            else if(işlemler.equals("2"))
+            else if(iÅŸlemler.equals("2"))
             {
-                System.out.print("çekmek istediğiniz tutar: ");
+                System.out.print("Ã§ekmek istediÄŸiniz tutar: ");
                 int tutar=scanner.nextInt();
-                scanner.nextLine();//döngü başa döner intten sonra line kullanıldığı için bu işlem uygulanır,
-                System.out.println(tutar+" tl çekildi..");
+                scanner.nextLine();//dÃ¶ngÃ¼ baÅŸa dÃ¶ner intten sonra line kullanÄ±ldÄ±ÄŸÄ± iÃ§in bu iÅŸlem uygulanÄ±r,
+                System.out.println(tutar+" tl Ã§ekildi..");
 
                 if(bakiye-tutar<0)
                 {
@@ -48,18 +48,18 @@ public class Main {
                     System.out.println("yeni bakiye: "+bakiye);
                 }
             }
-            else if(işlemler.equals("3"))
+            else if(iÅŸlemler.equals("3"))
             {
-                System.out.print("yatıracağınız toplam tutarı girin: ");
+                System.out.print("yatÄ±racaÄŸÄ±nÄ±z toplam tutarÄ± girin: ");
                int tutar= scanner.nextInt();
                 scanner.nextLine();
-                System.out.println(tutar+" tl yatırıldı..");
+                System.out.println(tutar+" tl yatÄ±rÄ±ldÄ±..");
                 bakiye+=tutar;
                 System.out.println("yeni bakiye: "+bakiye);
             }
             else
             {
-                System.out.println("gecersiz işlem..");
+                System.out.println("gecersiz iÅŸlem..");
                 System.out.println("tekrar deneyin..");
             }
 
